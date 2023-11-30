0ce8021994c14c0ce866292e72be009158a24776
Teacher <informatikvbg@gmail.com>
2023-11-30 15:24:33 +0300

    register.submit

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,11 @@
+const User = require("../models/user");
+
 exports.form = (req, res) => {
   res.render("registerForm", {});
 };
 
+exports.submit = (req, res, next) => {
+  if (!Игорь в базеи данных есть?)
+  
+  User.create(req.body.user, cb);
+};
--- routers/index_routers.js
+++ routers/index_routers.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 router.post("/", function (req, res) {});
 
 router.get("/register", register.form);
-router.post("/register", function (req, res) {});
+router.post("/register", register.submit);
 
 // app.post("/test", (req, res) => {
 //     addLine("Пинганули /test");
