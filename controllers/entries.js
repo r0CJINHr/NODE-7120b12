913f73e520f9942d0cf6470e904a3c5b119a29bb
Teacher <informatikvbg@gmail.com>
2024-02-23 22:36:19 +0300

    +technik2

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,6 +1,7 @@
 const logger = require('../logger/index');
 const Entry = require("../models/entry");
 const logger = require("../logger/index_logger");
+const logger = require("../logger/index_logger");
 
 exports.list = (req, res, next) => {
   Entry.selectAll((err, entries) => {
